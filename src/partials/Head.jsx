bf72d23ec2db11ec699191fd73c9272dafc47a17
Bariq Firjatullah <bariqfirjatullah1803>
2024-02-04 19:34:32 +0700

    Responsive Main Page

---
+++ src/partials/Head.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {Helmet} from "react-helmet";
+
+export default function Head() {
+    return (
+        <div>
+            <Helmet>
+                <title>Bariq Firjatullah</title>
+                <meta name="description"
+                      content="Saya adalah Bariq Firjatullah, seorang pengembang full-stack dengan keahlian di kedua sisi, baik frontend maupun backend. Jelajahi portofolio saya di bariqfirjatullah.pw, di mana saya menampilkan pengalaman dan proyek-proyek yang telah saya rancang dan kembangkan. Dari antarmuka pengguna yang responsif hingga fungsionalitas backend yang kuat, saya siap membawa solusi teknologi yang inovatif untuk proyek-proyek mendatang. Lihat keterampilan saya dan temukan bagaimana saya dapat meningkatkan proyek Anda."/>
+                <meta name="keywords"
+                      content="Pengembang Full-Stack, Portofolio Pengembang Web, Desain Antarmuka Pengguna (UI), Pengembangan Backend, Proyek Pengembangan Web, Full-Stack Developer Malang, Responsive Web Design, Keahlian Teknologi Web, Frontend dan Backend Developer, Inovasi Pengembangan Web, Bariq Firjatullah, Developer Indonesia, Developer Malang, Website Malang, Website Indonesia"/>
+                <meta name="author" content="Bariq Firjatullah"/>
+                <meta property="og:title" content="Bariq Firjatullah"/>
+                <meta property="og:description" content="Saya adalah Bariq Firjatullah, seorang pengembang full-stack dengan keahlian di kedua sisi, baik frontend maupun backend. Jelajahi portofolio saya di bariqfirjatullah.pw, di mana saya menampilkan pengalaman dan proyek-proyek yang telah saya rancang dan kembangkan. Dari antarmuka pengguna yang responsif hingga fungsionalitas backend yang kuat, saya siap membawa solusi teknologi yang inovatif untuk proyek-proyek mendatang. Lihat keterampilan saya dan temukan bagaimana saya dapat meningkatkan proyek Anda."/>
+                <meta property="og:image" content="https://bariqfirjatullah.pw/assets/profil-06bfc04e.webp"/>
+                <meta property="og:url" content="https://bariqfirjatullah.pw"/>
+                <meta name="twitter:title" content="Bariq Firjatullah"/>
+                <meta name="twitter:description" content="Saya adalah Bariq Firjatullah, seorang pengembang full-stack dengan keahlian di kedua sisi, baik frontend maupun backend. Jelajahi portofolio saya di bariqfirjatullah.pw, di mana saya menampilkan pengalaman dan proyek-proyek yang telah saya rancang dan kembangkan. Dari antarmuka pengguna yang responsif hingga fungsionalitas backend yang kuat, saya siap membawa solusi teknologi yang inovatif untuk proyek-proyek mendatang. Lihat keterampilan saya dan temukan bagaimana saya dapat meningkatkan proyek Anda."/>
+                <meta name="twitter:image" content="https://bariqfirjatullah.pw/assets/profil-06bfc04e.webp"/>
+                <meta name="twitter:card" content="https://bariqfirjatullah.pw/assets/profil-06bfc04e.webp"/>
+            </Helmet>
+            {/* Your component's code here */}
+        </div>
+    )
+}
